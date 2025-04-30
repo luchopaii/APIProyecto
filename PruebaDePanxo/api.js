4a8eebe8f4217fbeba28438bb3a8979c78a10176
luchopaii <luisvega200730008navarrete@gmail.com>
2025-04-30 01:04:36 -0400

    agrege un proyecto

---
+++ PruebaDePanxo/api.js
@@ -0,0 +1,18 @@
+function obtenerChiste(){
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(res => res.json())
+    .then(data =>{
+        const chiste = data.value;
+        document.getElementById('chisteInput1').value = chiste;
+    })
+    .catch(error =>{
+        console.error('Error al obtener el chiste:',error);
+    })
+}
+
+/**
+ * esta es para el boton limpiar
+ */
+document.getElementById('clear').onclick = function(){
+    document.getElementById('chisteInput1').value = '';
+}
